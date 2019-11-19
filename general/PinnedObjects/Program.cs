3dd9c9b94266ed7d245b01507344b9caec0a24c4
anton.shcherbyna <anton.shcherbyna@sigma.software>
2019-11-19 22:50:22 +0200

    Add pinned object

---
+++ general/PinnedObjects/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PinnedObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] bytes = new byte[128];
+            GCHandle gch = GCHandle.Alloc(bytes, GCHandleType.Pinned);
+            GC.Collect();
+            Console.WriteLine("Generation: " + GC.GetGeneration(bytes));
+            gch.Free();
+            GC.KeepAlive(bytes);
+            Console.ReadLine();
+        }
+    }
+}
