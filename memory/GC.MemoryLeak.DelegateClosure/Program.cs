e58cc0a45bdb6abc732326f60b2b006d9a34c7c0
Shcherbyna, Anton <anton.shcherbyna@sigma.software>
2019-11-27 19:19:44 +0200

    Done

--- memory/GC.MemoryLeak.DelegateClosure/Program.cs
+++ memory/GC.MemoryLeak.DelegateClosure/Program.cs
@@ -5,48 +5,33 @@ namespace GC.MemoryLeak.DelegateClosure
 {
     class Program
     {
-
+        private static Queue<Action> queue = new Queue<Action>();
         static void Main(string[] args)
         {
-            var r = new SamurayManager().WhoHasWeapon();
-            Console.WriteLine(r);
-        }
-
-        static Samuray LookForWeapon(List<Samuray> samurays, Weapon weapon)
-        {
-            return samurays.Find(s => s.Weapon == weapon);
-        }
-    }
-
-    class SamurayManager
-    {
-        readonly Weapon w1 = new Weapon();
-
-        private readonly List<Samuray> samurays;
-
-        public SamurayManager()
-        {
-            samurays = new List<Samuray> { new Samuray(new Weapon()), new Samuray(w1) };
-        }
-
-        public Samuray WhoHasWeapon()
-        {
-            return samurays.Find(d => d.Weapon == w1);
+            for (int i = 0; i < 4000; i++)
+            {
+                new Samuray(queue).Do();
+            }
         }
     }
 
     class Samuray
     {
-        public readonly Weapon Weapon;
+        private readonly Queue<Action> _jobQueue;
+        private string weapon = "Knife";
 
-        public Samuray(Weapon weapon)
+        public Samuray(Queue<Action> jobQueue)
         {
-            Weapon = weapon;
+            _jobQueue = jobQueue;
         }
-    }
 
-    class Weapon
-    {
-
+        public void Do()
+        {
+            _jobQueue.Enqueue(() =>
+            {
+                Console.WriteLine($"Fight with weapon {weapon}");
+                // do stuff 
+            });
+        }
     }
 }
