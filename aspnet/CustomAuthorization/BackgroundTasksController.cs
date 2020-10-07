8b202948f12a6a40acfef1ea0c20adcad184a90c
Anton <sag@vava.cars>
2020-10-07 16:45:48 +0300

    add authorization

---
+++ aspnet/CustomAuthorization/BackgroundTasksController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize("InternalApiToken")]
+    [ApiController]
+    public class BackgroundTasksController : ControllerBase
+    {
+        private readonly IBackgroundTask BackgroundTask;
+
+        public BackgroundTasksController(IBackgroundTask BackgroundTask)
+        {
+            this.BackgroundTask = BackgroundTask;
+        }
+
+        [HttpPost("updateStatus")]
+        public async Task<OkResult> UpdateStatus()
+        {
+            await BackgroundTask.UpdateStatusAsync();
+            return Ok();
+        }
+    }
+}
---
+++ aspnet/CustomAuthorization/InternalApiTokenAuthorizationHandler.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace API.BackgroundTasks
+{
+    public class InternalApiTokenAuthorizationHandler : AuthorizationHandler<InternalApiTokenRequirement>
+    {
+        private const string AuthorizationHeaderKey = "Authorization";
+        private readonly IHttpContextAccessor httpContextAccessor;
+        private readonly IConfiguration configuration;
+
+        public InternalApiTokenAuthorizationHandler(IHttpContextAccessor httpContextAccessor, IConfiguration configuration)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+            this.configuration = configuration;
+        }
+
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, InternalApiTokenRequirement requirement)
+        {
+            var token = configuration.GetValue<string>("BackgroundTask:AuthorizationToken");
+
+            if (IsAuthenticated(httpContextAccessor.HttpContext.Request.Headers, token))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private static bool IsAuthenticated(IHeaderDictionary headers, string token)
+            => headers.TryGetValue(AuthorizationHeaderKey, out var authorizationHeader)
+               && authorizationHeader.Count > 0
+               && !string.IsNullOrEmpty(authorizationHeader[0])
+               && token == authorizationHeader[0];
+    }
+
+    public class InternalApiTokenRequirement : IAuthorizationRequirement
+    {
+    }
+}
